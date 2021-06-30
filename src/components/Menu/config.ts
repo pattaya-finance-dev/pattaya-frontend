cca7cb0d5b4fdb103970da232103f86fb7f3f1fc
Anugoon Tangtriamchitman <anugoon_t@Anugoons-Mac-mini.local>
2021-06-30 16:35:19 +0700

    Initial commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,76 @@
+import { MenuEntry } from '@pattayaswap-dev-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://pantherswap.com/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://pantherswap.com/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://pantherswap.com/pools',
+  },
+  {
+    label: 'Referrals',
+    icon: 'ReferralIcon',
+    href: 'https://pantherswap.com/referrals',
+  },
+  {
+    label: 'Audits',
+    icon: 'ShieldIcon',
+    href: 'https://docs.pantherswap.com/security/audits',
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      // {
+      //   label: 'Voting',
+      //   href: 'https://voting.pancakeswap.finance',
+      // },
+      {
+        label: 'Github',
+        href: 'https://github.com/pantherswap',
+      },
+      {
+        label: 'Docs',
+        href: 'https://docs.pantherswap.com',
+      },
+      {
+        label: 'Roadmap',
+        href: 'https://docs.pantherswap.com/roadmap',
+      },
+      {
+        label: 'Blog',
+        href: 'https://pantherswap.medium.com',
+      },
+      {
+        label: 'Voting',
+        href: 'https://voting.pantherswap.com/',
+      },
+    ],
+  },
+]
+
+export default config
